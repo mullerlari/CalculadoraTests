d8e1d6b6d6044237e4dc9edd237bc6a3c8836b38
Larissa Muller <larissa.muller@outlook.com.br>
2024-05-10 22:36:24 -0300

    testes unitários

---
+++ NewTalents/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewTalents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
